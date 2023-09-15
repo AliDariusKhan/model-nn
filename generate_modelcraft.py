07f14328c2a493990d0f1ed6c3e019fda04cb987
Ali Darius Khan <Ali.Khan.Physics@gmail.com>
2023-09-15 09:21:09 +0100

    print output of modelcraft

--- generate_modelcraft.py
+++ generate_modelcraft.py
@@ -1,21 +1,6 @@
 import os
-import requests
 import subprocess
 
-def fetch_mtz(pdb_id, output_dir='mtzs_for_modelcraft'):
-    mtz_path = os.path.join(output_dir, f"{pdb_id}.mtz")
-    if os.path.exists(mtz_path):
-        print(f"MTZ for {pdb_id} already exists. Skipping download.")
-        return
-
-    base_url = "https://edmaps.rcsb.org/coefficients"
-    mtz_url = f"{base_url}/{pdb_id}.mtz"
-    response = requests.get(mtz_url, stream=True)
-    response.raise_for_status()
-    os.makedirs(output_dir, exist_ok=True)
-    with open(mtz_path, 'wb') as f:
-        for chunk in response.iter_content(chunk_size=8192):
-            f.write(chunk)
 
 def generate_contents_json(pdb_id, output_dir='contents_files'):
     contents_json_path = os.path.join(output_dir, f"{pdb_id}_contents.json")
@@ -25,9 +10,20 @@ def generate_contents_json(pdb_id, output_dir='contents_files'):
 
     cmd = ["modelcraft-contents", pdb_id, contents_json_path]
     os.makedirs(output_dir, exist_ok=True)
-    result = subprocess.run(cmd, capture_output=True, text=True)
-    if result.returncode != 0:
-        print(f"Failed to generate contents JSON for {pdb_id}: {result.stderr}")
+    with subprocess.Popen(cmd, stdout=subprocess.PIPE, stderr=subprocess.PIPE, text=True) as process:
+        while True:
+            output = process.stdout.readline()
+            if output == '' and process.poll() is not None:
+                break
+            if output:
+                print(output.strip())
+        if process.returncode != 0:
+            for line in process.stderr:
+                print(line.strip())
+            raise RuntimeError(f"Failed to generate contents JSON for {pdb_id}")
+
+    if not os.path.exists(contents_json_path) or os.path.getsize(contents_json_path) == 0:
+        raise RuntimeError(f"Failed to generate contents JSON for {pdb_id}: File not created or empty")
 
 def execute_modelcraft(pdb_id):
     directory_path = os.path.join('modelcraft_outputs', pdb_id)
@@ -45,11 +41,11 @@ def execute_modelcraft(pdb_id):
     data_path = os.path.join('mtzs_for_modelcraft', f"{pdb_id}.mtz")
 
     phase_args = [
+        "PHIC_ALL_LS,FOM",
         "PHIC,FOM",
         "PHIC_ALL,FOM",
         "PHWT,FOM",
         "PHDELWT,FOM",
-        "PHIC_ALL_LS,FOM",
     ]
 
     for phase_arg in phase_args:
@@ -69,23 +65,39 @@ def execute_modelcraft(pdb_id):
             "--disable-side-chain-fixing",
             "--phases", phase_arg,
         ]
-        result = subprocess.run(cmd, capture_output=True, text=True)
-        if result.returncode == 0:
-            break
-        else:
-            print(f"Failed to execute modelcraft for {pdb_id} with phases '{phase_arg}': {result.stderr}")
+        with subprocess.Popen(cmd, stdout=subprocess.PIPE, stderr=subprocess.PIPE, text=True) as process:
+            while True:
+                output = process.stdout.readline()
+                if output == '' and process.poll() is not None:
+                    break
+                if output:
+                    print(output.strip())
+            if process.returncode != 0:
+                for line in process.stderr:
+                    print(line.strip())
+                print(f"Failed to execute modelcraft for {pdb_id} with phases '{phase_arg}': {process.stderr.read().strip()}")
 
-    if result.returncode != 0:
-        print(f"Failed to execute modelcraft for {pdb_id}: exhausted all phase options")
+    for expected_file in expected_files:
+        if not os.path.exists(expected_file) or os.path.getsize(expected_file) == 0:
+            raise RuntimeError(f"Failed to execute modelcraft for {pdb_id}: Output file '{expected_file}' not created or empty")
 
 def main():
-    with open('./input_pdb_list.txt', 'r') as f:
-        pdb_ids = [pid.strip() for pid in f.read().lower().split(',')]
+    with open('successful_mtz_downloads.txt', 'r') as f:
+        pdb_ids = f.read().splitlines()
+    
+    successful_pdb_ids = []
 
-    for pdb_id in reversed(pdb_ids):
-        fetch_mtz(pdb_id)
-        generate_contents_json(pdb_id)
-        execute_modelcraft(pdb_id)
+    for pdb_id in pdb_ids:
+        try:
+            generate_contents_json(pdb_id)
+            execute_modelcraft(pdb_id)
+            successful_pdb_ids.append(pdb_id)
+        except:
+            print(f"Failed to generate modelcraft for {pdb_id}")
+
+    with open('successful_modelcraft_generations.txt', 'w') as f:
+        for pdb_id in successful_pdb_ids:
+            f.write(f"{pdb_id}\n")
 
 if __name__ == "__main__":
     main()
