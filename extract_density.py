9ff496e4d966c73059775a310af14c5e1c9abf23
Ali Darius Khan <Ali.Khan.Physics@gmail.com>
2023-09-20 14:27:39 +0100

    tortoize data

--- extract_density.py
+++ extract_density.py
@@ -1,8 +1,13 @@
 import os
 import gemmi
 import numpy as np
+import random
+import subprocess
+import json
+import tensorflow as tf
+import requests
 
-N_GRID = 8
+N_GRID = 32
 MAPS = [("FWT", "PHWT"), ("DELFWT", "PHDELWT")]
 
 def gemmi_position_to_np_array(gemmi_pos):
@@ -20,53 +25,218 @@ def get_grid_basis(residue):
     y_norm = np.cross(x_norm, z_norm)
     return origin, x_norm, y_norm, z_norm
 
-def density_map_grid(residue, density_map, spacing=1.0):
+def density_map_grid(residue, density_map, spacing=0.5):
     origin, x, y, z = get_grid_basis(residue)
     grid_corner = origin - ((N_GRID - 1) * spacing / 2) * (x + y + z)
 
     density_map_grid_values = np.zeros((N_GRID, N_GRID, N_GRID), dtype=np.float32)
     
     transform = gemmi.Transform()
-    transform.mat.fromlist(np.column_stack([x, y, z]))
+    transform.mat.fromlist(spacing * np.column_stack([x, y, z]))
     transform.vec.fromlist(grid_corner)
 
     density_map.interpolate_values(density_map_grid_values, transform)
     return density_map_grid_values
 
-def residue_density_map_generator(pdb_id):
-    mtz = gemmi.read_mtz_file(os.path.join('modelcraft_outputs', pdb_id, 'modelcraft.mtz'))
-    density_maps = [mtz.transform_f_phi_to_map(amplitude, phase) for amplitude, phase in MAPS]
-    for density_map in density_maps:
-        density_map.normalize()
+def pdb_id_generator(train_or_test):
+    with open(f'./{train_or_test}_pdb_ids.txt', 'r') as f:
+        pdb_ids = f.read().splitlines()
+    for pdb_id in pdb_ids:
+        yield pdb_id
 
-    model_structure_path = os.path.join('modelcraft_outputs', f"{pdb_id}", "modelcraft.cif")
-    model_structure = gemmi.read_structure(model_structure_path)
+def get_tortoize_data(pdb_id, cif_path, pdb_dir="modelcraft_pdb"):
+    os.makedirs(pdb_dir, exist_ok=True)
+    pdb_path = os.path.join(pdb_dir, f"{pdb_id}.pdb")    
+    subprocess.Popen(
+            f'gemmi convert {cif_path} {pdb_path}', shell=True)
 
-    for chain in model_structure[0]:
-        for residue in chain:
-            if not gemmi.find_tabulated_residue(residue.name).is_amino_acid():
-                continue
+    tortoize_process = subprocess.Popen(
+        f'tortoize {pdb_path}',
+        shell=True,
+        stdout=subprocess.PIPE)
+    tortoize_output = tortoize_process.communicate()[0]
+    print("tortoize_output here", tortoize_output)
+    tortoize_dict = json.loads(tortoize_output)
+    residues = tortoize_dict["model"]["1"]["residues"]
 
-            all_map_values = []
-            for density_map in density_maps:
-                map_values = density_map_grid(residue, density_map).reshape(*3*N_GRID, 1)
-                all_map_values.append(map_values)
+    rama_z_data = dict()
+    for res in residues:
+        chain_rama_z_data = rama_z_data.setdefault(res['pdb']['strandID'], {})
+        chain_rama_z_data[res['pdb']['seqNum']] = res['ramachandran']['z-score']
+    
+    return rama_z_data
 
-            yield np.concatenate(all_map_values, axis=-1)
+def get_reference_structure(pdb_id, output_dir='pdb_files'):
+    os.makedirs(output_dir, exist_ok=True)
+    path = os.path.join(output_dir, f"{pdb_id}.pdb")
+    if os.path.exists(jarvis_path := f'/old_vault/pdb/pdb{pdb_id}.ent'):
+        print(f"PDB for {pdb_id} found on Jarvis")
+        path = jarvis_path
+    elif os.path.exists(path):
+        print(f"PDB for {pdb_id} already downloaded")
+    else:
+        base_url = "https://files.rcsb.org/download"
+        pdb_url = f"{base_url}/{pdb_id}.pdb"
+        response = requests.get(pdb_url, stream=True)
+        response.raise_for_status()
+        with open(path, 'wb') as f:
+            for chunk in response.iter_content(chunk_size=8192):
+                f.write(chunk)    
+    return gemmi.read_structure(path)
 
-def main():
+def residue_density_map_generator(train_or_test):
+    pdb_ids = pdb_id_generator(train_or_test)
+    for pdb_id in pdb_ids:
+        mtz = gemmi.read_mtz_file(os.path.join('modelcraft_outputs', pdb_id, 'modelcraft.mtz'))
+        density_maps = [mtz.transform_f_phi_to_map(amplitude, phase) for amplitude, phase in MAPS]
+
+        for density_map in density_maps:
+            density_map.normalize()
+
+        cif_path = os.path.join('modelcraft_outputs', f"{pdb_id}", "modelcraft.cif")
+        tortoize_data = get_tortoize_data(pdb_id, cif_path)
+
+        model_structure = gemmi.read_structure(cif_path)
+        reference_structure = get_reference_structure(pdb_id)     
+
+        for chain in model_structure[0]:
+            chain_rama_z_data = tortoize_data[chain.name]
+            for residue in chain:
+                if residue.seqid.num not in chain_rama_z_data:
+                    continue
+                rama_z_score = chain_rama_z_data[residue.seqid.num]
+                
+                if not gemmi.find_tabulated_residue(residue.name).is_amino_acid():
+                    continue
+
+                all_map_values = []
+                for density_map in density_maps:
+                    map_values = density_map_grid(residue, density_map).reshape(N_GRID, N_GRID, N_GRID, 1)
+                    all_map_values.append(map_values)
+
+                yield np.concatenate(all_map_values, axis=-1), np.array([rama_z_score])
+
+def create_model():
+    x = inputs = tf.keras.Input(shape=(N_GRID, N_GRID, N_GRID, 2))
+    _downsampling_args = {
+        "padding": "same",
+        "use_bias": False,
+        "kernel_size": 3,
+        "strides": 1,
+    }
+
+    filter_list = [32, 64, 128]
+    for filters in filter_list:
+        x = tf.keras.layers.Conv3D(filters, **_downsampling_args)(x)
+        x = tf.keras.layers.BatchNormalization()(x)
+        x = tf.keras.layers.ReLU()(x)
+        x = tf.keras.layers.MaxPool3D(2)(x)
+
+    x = tf.keras.layers.Flatten()(x)
+    x = tf.keras.layers.Dense(128)(x)
+    x = tf.keras.layers.Dense(64)(x)
+    x = tf.keras.layers.Dense(32)(x)
+    x = tf.keras.layers.Dense(16)(x)
+    x = tf.keras.layers.Dense(8)(x)
+    output = tf.keras.layers.Dense(1)(x)
+    return tf.keras.Model(inputs=inputs, outputs=output)
+
+def train():
+    train_gen = residue_density_map_generator("train")
+    test_gen = residue_density_map_generator("test")
+    input = tf.TensorSpec(shape=(N_GRID, N_GRID, N_GRID, 2), dtype=tf.float32)
+    output = tf.TensorSpec(shape=(1), dtype=tf.float32)
+
+    train_dataset = tf.data.Dataset.from_generator(
+        lambda: train_gen, output_signature=(input, output)
+    )
+    test_dataset = tf.data.Dataset.from_generator(
+        lambda: test_gen, output_signature=(input, output)
+    )
+
+    epochs: int = 100
+    batch_size: int = 8
+    steps_per_epoch: int = 10000
+    validation_steps: int = 1000
+    name: str = "test_1"
+
+    train_dataset = train_dataset.repeat(epochs).batch(batch_size=batch_size)
+    test_dataset = test_dataset.repeat(epochs).batch(batch_size=batch_size)
+
+    model = create_model()
+    model.summary()
+
+    model.compile(optimizer=tf.optimizers.Adam(learning_rate=1e-4), loss="mse",  metrics=['mse'])
+
+    logger = tf.keras.callbacks.CSVLogger(f"train_{name}.csv", append=True)
+    reduce_lr_on_plat = tf.keras.callbacks.ReduceLROnPlateau(
+        monitor="val_loss",
+        factor=0.8,
+        patience=5,
+        verbose=1,
+        mode="auto",
+        cooldown=5,
+        min_lr=1e-7,
+    )
+    weight_path: str = f"models/{name}.best.hdf5"
+
+    checkpoint = tf.keras.callbacks.ModelCheckpoint(
+        weight_path,
+        monitor="val_loss",
+        verbose=1,
+        save_best_only=True,
+        mode="min",
+        save_weights_only=False,
+    )
+    
+    tensorboard_callback = tf.keras.callbacks.TensorBoard(
+        log_dir=f"./logs/{name}", histogram_freq=1, profile_batch=(10, 30)
+    )
+    
+    callbacks_list = [
+        checkpoint,
+        # reduce_lr_on_plat,
+        # TqdmCallback(verbose=2),
+        tensorboard_callback,
+    ]
+
+    model.fit(
+        train_dataset,
+        epochs=epochs,
+        steps_per_epoch=steps_per_epoch,
+        validation_data=test_dataset,
+        validation_steps=validation_steps,
+        callbacks=callbacks_list,
+        verbose=1,
+        use_multiprocessing=True,
+    )
+
+def train_test_split(train_proportion=0.8):
     modelcraft_folder = 'modelcraft_outputs'
     pdb_ids = []
-
     for root, _, files in os.walk(modelcraft_folder):
         if any(file.endswith('.cif') for file in files):
-            pdb_ids.append(root)
+            pdb_ids.append(root.split('/')[1])
 
-    for pdb_id in pdb_ids:
-        try:
-            residue_density_map_generator(pdb_id)
-        except:
-            print(f"Failed to generate modelcraft for {pdb_id}")
+    random.shuffle(pdb_ids)
+    num_train = int(len(pdb_ids) * train_proportion)
+    if num_train == 0:
+        num_train = 1
+    elif num_train == len(pdb_ids):
+        num_train = len(pdb_ids) - 1
+
+    train_ids = pdb_ids[:num_train]
+    test_ids = pdb_ids[num_train:]
+
+    for train_or_test, pdb_ids in [("train", train_ids), ("test", test_ids)]:
+        for pdb_id in pdb_ids:
+            with open(f'{train_or_test}_pdb_ids.txt', 'w') as file:
+                file.write(f"{pdb_id}\n")
+
+def main():
+    train_test_split()
+    train()
 
 if __name__ == "__main__":
     main()
+    
